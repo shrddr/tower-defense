a27da8c0dc134f25ba95fe4a3fbc510bfd228558
shrddr <qwerty.zigzag@gmail.com>
2017-03-18 11:58:01 +0300

    Walking sphere using NavMesh

---
+++ Assets/Scripts/ClickToWalk.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToWalk : MonoBehaviour
+{
+    public Vector3 ShowDestination;
+    private NavMeshAgent _navMeshAgent;
+
+	void Start()
+	{
+	    _navMeshAgent = GetComponent<NavMeshAgent>();
+	    _navMeshAgent.destination = transform.position;
+	}
+	
+	void Update()
+	{
+	    if (Input.GetMouseButton(0))
+	    {
+	        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	        RaycastHit hit;
+
+	        if (Physics.Raycast(ray, out hit))
+	        {
+	            _navMeshAgent.SetDestination(hit.point);
+	        }
+	    }    
+	}
+}
